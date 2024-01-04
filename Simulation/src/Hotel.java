628046d83eed735bc666c25d862f2315ee2258dd
Solomon <soul_tefera@yahoo.com>
2024-01-03 23:31:17 -0600

    update

--- Simulation/src/Hotel.java
+++ Simulation/src/Hotel.java
@@ -1,50 +1,94 @@
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.Semaphore;
-
+/**
+ * This class is used to simulate hotel, guest, bellhop, and front desk employee. 
+ * It creates 25 guests, 2 bellhop, and 2 front desk employee threads
+ * @author Solomon Demisse
+ * @since November 30, 2023
+ */
 public class Hotel {
-	private static final  int guest_count = 25 ;
-	public  static Queue<Thread> each_guest_thread = new LinkedList<>(); 
+	// maximum number of guests
+	private static final int max_guest = 25; 
+	public static Guest guests [] = new Guest[max_guest]; 
+	public static BellHop bellhops[] = new BellHop[2]; 
+	public static Front_Desk frontdesk_employees[] = new Front_Desk[2]; 
+	//guest waiting in the queue to check in
+	public static Queue<Guest> guest_wait_frontdesk = new LinkedList<>(); 
 	
-	static Semaphore wait_main_thread = new Semaphore(-24, true); 
+	// guest waiting in the queue to get help for their luggage. 
+	public static Queue<Guest> guest_wait_bellhop = new LinkedList<>(); 
+	
+	// room number starts from 100
+	public static int room_numbers = 100; 
+	// number of guests who has more than two bags
+	public static int guest_morethan_two_bags; 
 
-	public static void main(String[] args) throws InterruptedException {
+	public static Semaphore guest_morethan_two_bags_semaphore;  
+	public static Semaphore guest_semaphore = new Semaphore(25);
+	public static Semaphore frontdesk_wait_guest = new Semaphore(0, true); 
+	public static Semaphore guest_wait_front_desk = new Semaphore(0, true); 
+	 
+	
+	public static Semaphore bellhop_wait_guest = new Semaphore(0); 
+	public static Semaphore bellhop_wait_guest_One = new Semaphore(0); 
+	public static Semaphore guest_wait_bellhops = new Semaphore(0); 
+	public static Semaphore guest_wait_bellhops_One = new Semaphore(0); 
+	
+	public static void main(String[] args)
+	{
 		System.out.println("Simulation starts"); 
-		// create Front_Desk  objects
-		Front_Desk front_desk_obj_one = new Front_Desk(); 
-		Front_Desk front_desk_obj_two = new Front_Desk(); 
+		// create guest objects
+		for(int i = 0; i < 25; i++)
+			guests[i] = new Guest(i);
 		
-		// create Front_Desk  threads
-		Thread front_desk_thread_one = new Thread(front_desk_obj_one, "0"); 
-		Thread front_desk_thread_two = new Thread(front_desk_obj_two, "1"); 
-		// start Front_desk threads
-		front_desk_thread_one.start(); 
-		front_desk_thread_two.start();
+		guest_morethan_two_bags_semaphore = new Semaphore(guest_morethan_two_bags); 
 		
-		// Create BellHop objects
-		BellHop bellhop_obj_one = new BellHop(); 
-		BellHop bellhop_obj_two = new BellHop(); 
-		
-		// Create BellHop threads
-		Thread bellhop_thread_one = new Thread(bellhop_obj_one, "0"); 
-		Thread bellhop_thread_two = new Thread(bellhop_obj_two, "1"); 
-		
-		// starts bellhop threads
-		bellhop_thread_one.start();
-		bellhop_thread_two.start(); 
-		
-		for(int i = 0; i < guest_count; i++)
+		//create front desk employee objects and start thread
+		for(int i = 0; i < 2; i++)
 		{
-			Guest guest_obj = new Guest(); 
-			Thread guest_thread = new Thread(guest_obj, String.valueOf(i)); 
-			//each_guest_thread.add(guest_thread); 
-			guest_thread.start();
+			frontdesk_employees[i] = new Front_Desk(i); 
+			frontdesk_employees[i].start();
 		}
 		
-		wait_main_thread.acquire();
-		System.out.println("Simulation ends"); 
+        //create bellhop objects and start thread
+		for(int i = 0; i < 2; i++)
+		{
+			bellhops[i] = new BellHop(i); 
+			bellhops[i].start();
+		}
+	
+
+		// start guest threads
+		for(Guest a_guest: guests)
+			a_guest.start();
+		
+	
+		try
+		{
+			// joining guest threads
+			for(Guest guest_thread: guests)
+			guest_thread.join();
+			
+			//joining bellhop and front desk
+			for(int i =0; i < 2; i++)
+			{
+			  frontdesk_employees[i].join();
+			  bellhops[i].join(); 
+			}
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			System.out.println("Simulation ends"); 
+		}
+		
 		
 		
 	}
-	
+		
 }
